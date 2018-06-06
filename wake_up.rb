512391ed74202baaebc931998383b326c128d801
Kaitlym <azkatie2014@gmail.com>
2018-06-06 22:40:21 +0000

    Done.

---
+++ wake_up.rb
@@ -0,0 +1,16 @@
+def wake_up
+puts "hear alarm, press snooze and..."
+5.times do
+  puts "zzzzzzzzzzzzz..."
+end
+3.times do
+  puts "meow..."
+end
+puts "MEOWWWWWWW!!!
+I'm awake, awake!
+
+...mean kitty."
+  
+end
+
+wake_up
